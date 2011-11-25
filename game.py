b930c53e45aff87c53495dccc60918fa67f4bd25
James Keene <outofthemadness@gmail.com>
2011-11-25 15:24:46 -0800

    added find_type and getting closer now to an actual logical idea
    When I get back home, I'll have more time to concentrate on this, I'm
    sure of it.

---
+++ game.py
@@ -0,0 +1,6 @@
+from cards import Card
+from hand import Hand
+
+for i in xrange(4):
+    royal_flush = [Card(i,10 + p) for p in xrange(5)]
+
--- hand.py
+++ hand.py
@@ -3,13 +3,16 @@ from cards import suitList as suits
 from cards import rankList as ranks
 from operator import and_ as ans
 from operator import eq
-royal = set([10, "J", "Q", "K", "A"])
-hand_types = {"HC":ranks, "1P":ranks, "":ranks, "":ranks[5:], "":ranks[5:],  }
+hand_types =   {"HC":ranks, "1P":ranks,
+                "2P":ranks, "3K":ranks[5:],
+                "SR":ranks[5:],"FL":ranks[5:],
+                "FH":(ranks[5:], ranks), "4K":ranks,
+                "SF":ranks[5:], "RF":suits
+                }
 
 class Hand(object):
     """a poker hand"""
     def __init__(self, cards):
-        super(Hand, self).__init__()
         self.cards = cards
         self.rank = None
 
@@ -25,6 +28,18 @@ class Hand(object):
         """returns True if this Hand beats hand"""
         return self.rank > hand.rank
 
+    def find_type(self):
+        """figure out what type of hand this is."""
+        if self.is_royal():
+            return "RF"
+        elif self.is_a_straight():
+            return "SR"
+        elif self.is_a_flush():
+            return "FL"
+        elif self.straight_flush():
+            pass
+
+
     def is_royal(self):
         """docstring for is_royal"""
         return reduce(ans, (c.face() for c in self.cards))
@@ -33,7 +48,6 @@ class Hand(object):
         """true if it's a straight, false otherwise"""
         lin = sorted(R.suit for R in self.cards)
         return lin == range(min(lin), max(lin) + 1)
-
 
     def is_a_flush(self):
         """true if it's a flush, false otherwise"""
@@ -46,6 +60,7 @@ class Hand(object):
     def royal_flush(self):
         """docstring for royal_flush"""
         return self.is_a_straight() and self.is_a_flush() and self.is_royal()
+
 
 class HandTest(object):
     """Should register as a royal flush"""
